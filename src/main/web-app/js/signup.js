f16b2cdbdc478c35b2dceabd23329c14475079bc
PhoebeBaham <phoebe.baham@gmail.com>
2014-07-21 13:15:12 -0500

    Created two functions to check if user entered in information and to see if they went over the max length. Still an alert pops up to say what is wrong but is no longer as specific.

--- src/main/web-app/js/signup.js
+++ src/main/web-app/js/signup.js
@@ -1,9 +1,7 @@
 	
 	function validateMyForm(){
-
-		 var valid = true; 
 		 var validationMsg = "Please correct the following errors: \r\n";
-		 var firstName = document.getElementById('firstnam');
+		 var firstnam = document.getElementById('firstnam');
 		 var middleName = document.getElementById('middlenam');
 		 var lastName = document.getElementById('lastnam');
 		 var month = document.getElementById('monthE');
@@ -17,200 +15,143 @@
 		 var username = document.getElementById('username');
 		 var password = document.getElementById('password');
 		 var cPassword = document.getElementById('cPassword');
+		
+		 validateFirstName();
+		 validateMiddleName();
+		 validateLastName();
+		 validateMonth();
+		 validateDay();
+		 validateYear();
+		 validateCountry();
+		 validateState();
+		 validateCity();
+		 validateZipcode();
+		 validateEmail();
+		 validateUsername();
+		 validatePassword();
+		 validateConPassword();
 		 
-		 
-		if(firstName.value.length == 0){
-			validationMsg = validationMsg + "Please fill out first name\r\n";
-			valid = false;
-		} else if(firstName.value.length > 30){
-			validationMsg = validationMsg + "Please enter less characters into first name\r\n";
-			valid = false;
+		 function validateFirstName(){
+			 if (validateRequired(firstnam.value.length, 0)== false)
+			 {
+			       
+			 } 
+			 else if(overMax(firstnam.value.length, 30)== false){
+
+			 }	
+	       }
+		 function validateMiddleName(){
+			 if(validateRequired(middleName.value.length, 0)== false){
 			
-		}
-		if(middleName.value.length == 0){
-			validationMsg = validationMsg + "Please fill out middle name\r\n";
-			valid = false;
-		} else if(middleName.value.length > 30){
-			validationMsg = validationMsg + "Please enter less characters into middle name\r\n";
-			valid = false;
+			 } 	else if(overMax(middleName.value.length, 30)== false){
 			
-		}
-		if(lastName.value.length == 0){
-			validationMsg = validationMsg + "Please fill out last name\r\n";
-			valid = false;
-		} else if(lastName.value.length > 30){
-			validationMsg = validationMsg + "Please enter less characters into last name\r\n";
-			valid = false;
+			 }
+		 } 
+		 function validateLastName(){
+			 if(validateRequired(lastName.value.length, 0)== false){
 			
+			 } else if(overMax(lastName.value.length, 30)== false){
+			
+			 }
+		 }
+			 
+		function validateMonth(){ 
+			 if(validateRequired(month.value.length, 0)== false){
+			
+			 }else if(overMax(month.value.length, 8)== false){
+			
+			
+			 }
 		}
+			 
+		function validateDay(){	 
+			 if(validateRequired(day.value.length, 0)== false){
+			
+			 }else if(overMax(day.value.length, 2)== false){
+			 	
+			 }
+		}
+			 
+		function validateYear(){	 
+			 if(validateRequired(year.value.length, 0)== false){
+			
+			 }else if(overMax(year.value.length, 4)== false){
 		
-		if(month.value.length == 0){
-			validationMsg = validationMsg + "Please fill out the month of your birthday\r\n";
-			valid = false;
-		}else if(month.value.length > 8){
-			validationMsg = validationMsg + "Please enter less characters into month of your birthday\r\n";
-			valid = false;
+			 }
 		}
+			 
+		function validateCountry(){	 
+			 if(validateRequired(countrySelect.value, 'choose')== false){
+			
+			 }
+			 
+		}	 
+		function validateState(){
+			 if(validateRequired(stateSelect.value, 'choose')== false){
+			
+			 }
+			 
+		}
+		function validateCity(){
+			 	if(validateRequired(city.value.length, 0)== false){
+			
+			 }else if(overMax(city.value.length, 30)== false){
+
+			 }
+		}	 	
+		function validateZipcode(){	 	
+			 if(validateRequired(zipcode.value.length, 0)== false){
+			
+			 }else if(overMax(zipcode.value.length, 5)== false){
+			
+			 }
+		}	 
+		function validateEmail(){	 
+			 if(validateRequired(email.value.length, 0)== false){
 		
-		if(day.value.length == 0){
-			validationMsg = validationMsg + "Please fill out the day of your birthday\r\n";
-			valid = false;
-		}else if(day.value.length > 2){
-			validationMsg = validationMsg + "Please enter less characters into the day of your birthday\r\n";
-			valid = false;
+			 }
+			 
 		}
+		function validateUsername(){
+			 if(validateRequired(username.value.length, 0)== false){
+			
+			
+			 }else if(overMax(username.value.length, 15)== false){
+			
+			 }
+		}
+		function validatePassword(){	 
+			 if(validateRequired(password.value.length, 0)== false){
+			
+			
+			 }else if(overMax(password.value.length, 15)== false){
+			
+			 }
+		}	 
+		function validateConPassword(){	 
+			 if(validateRequired(cPassword.value.length, 0)== false){
+			
+			 }else if(password.value != cPassword.value){
+			
+			 } 
+		}	 
+			 
 		
-		if(year.value.length == 0){
-			validationMsg = validationMsg + "Please fill out the year of your birthday\r\n";
-			valid = false;
-		}else if(year.value.length > 4){
-			validationMsg = validationMsg + "Please enter less characters into the year of your birthday \r\n";
-			valid = false;
-		}
-		
-		if(countrySelect.value == 'choose'){
-			validationMsg = validationMsg + "Please pick a country\r\n";
-			valid = false;
-		}
-		
-		if(stateSelect.value == 'choose'){
-			validationMsg = validationMsg + "Please pick a state\r\n";
-			valid = false;
-		}
-		if(city.value.length == 0){
-			validationMsg = validationMsg + "Please put your current city\r\n";
-			valid = false;
-		}else if(city.value.length > 30){
-			validationMsg = validationMsg + "Please enter less characters into your current city \r\n";
-			valid = false;
-		}
-		if(zipcode.value.length == 0){
-			validationMsg = validationMsg + "Please put your current zipcode\r\n";
-			valid = false;
-		}else if(zipcode.value.length > 5){
-			validationMsg = validationMsg + "Please enter less characters into the your current zipcode \r\n";
-			valid = false;
-		}
-		if(email.value.length == 0){
-			validationMsg = validationMsg + "Please fill out your email\r\n";
-			valid = false;
-		}
-		if(username.value.length == 0){
-			validationMsg = validationMsg + "Please create a username\r\n";
-			valid = false;
-		}else if(username.value.length > 15){
-			validationMsg = validationMsg + "Please enter less characters for you username \r\n";
-			valid = false;
-		}
-		if(password.value.length == 0){
-			validationMsg = validationMsg + "Please create a password\r\n";
-			valid = false;
-		}else if(password.value.length > 15){
-			validationMsg = validationMsg + "Please enter less characters for your password \r\n";
-			valid = false;
-		}
-		if(cPassword.value.length == 0){
-			validationMsg = validationMsg + "Please confirm your password\r\n";
-			valid = false;
-		}else if(password.value != cPassword.value){
-			validationMsg = validationMsg + "Please confirm you password\r\n";
-			valid = false;
-		}
-		
-		if(valid == false){
+		if(result == false){
 			alert(validationMsg);
 		}
 		
 	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-/*function validateNameElement(){
-	if(isFirstNameValid = false){
-		alert('error:fill out more');
+		function validateRequired(valAndLen, actualValue){
+			if (valAndLen == actualValue){
+				result = false;
+				validationMsg = validationMsg + "Please fill out more information\r\n";
+			}
+		}
+		function overMax(variableVal,maxVal){
+			if(variableVal > maxVal){
+			result = false;
+			validationMsg = validationMsg + "Please enter in less characters\r\n" ;
+			}
+		}
 	}
-	else{
-		validateFirstName();
-	}
-}
-function validateFirstName(){
-	if( > 30){
-		alert('error:too many characters in first name');
-	}
-}		
-		var middleNameElement = document.getElementById("middlenam");
-		
-		if(x.length == 0 ){
-			alert('please enter in more information');
-		}
-		if(x.length > 30){
-			alert('you have entered too many letters for your name');
-		}
-		if(y.length == 0){
-			alert('please enter in more information');
-		}
-		if(y.length > 30){
-			alert('you have entered too many letters for your name');
-		}
-		
-		var firstNameValid = isFirstNameValid();
-
-function isFirstNameValid(){
-	var firstNameElement = document.getElementById("firstnam");
-	isEmpty(firstNameElement.length);
-	
-}
-*/
-
-
-
-
-
-
-
-
-
