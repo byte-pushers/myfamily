cf6adc3ad11fbdbbd1063bc2af88dedad2ad9530
PhoebeBaham <phoebe.baham@gmail.com>
2014-07-22 12:17:51 -0500

    Committing due to merge issues, however new fucntions were created for if the user entered in too less of characters and also a fucntion that makes sure the user entered in a valid month.

--- src/main/web-app/js/signup.js
+++ src/main/web-app/js/signup.js
@@ -1,165 +1,5 @@
 	
 	function validateMyForm(){
-<<<<<<< HEAD
-
-		 var valid = true; 
-		 var validationMsg = "Please correct the following errors: \r\n";
-		
-		if(document.getElementById('firstnam').value.length == 0){
-			validationMsg = validationMsg + "Please fill out first name\r\n";
-			valid = false;
-			
-		}
-		if(document.getElementById('middlenam').value.length == 0){
-			validationMsg = validationMsg + "Please fill out middle name\r\n";
-			valid = false;
-		}
-		if(document.getElementById('lastnam').value.length == 0){
-			validationMsg = validationMsg + "Please fill out last name\r\n";
-			valid = false;
-		}
-		if(document.getElementById('monthE').value.length == 0){
-			validationMsg = validationMsg + "Please fill out the month of your birthday\r\n";
-			valid = false;
-		}
-		if(document.getElementById('dayE').value.length == 0){
-			validationMsg = validationMsg + "Please fill out the day of your birthday\r\n";
-			valid = false;
-		}
-		if(document.getElementById('yearE').value.length == 0){
-			validationMsg = validationMsg + "Please fill out the year of your birthday\r\n";
-			valid = false;
-		}
-		if(document.getElementById('countrySelect').value == 'choose'){
-			validationMsg = validationMsg + "Please pick a country\r\n";
-			valid = false;
-		}
-		if(document.getElementById('stateSelect').value == 'choose'){
-			validationMsg = validationMsg + "Please pick a state\r\n";
-			valid = false;
-		}
-		if(document.getElementById('city').value.length == 0){
-			validationMsg = validationMsg + "Please put your current city\r\n";
-			valid = false;
-		}
-		if(document.getElementById('zipcode').value.length == 0){
-			validationMsg = validationMsg + "Please put your current zipcode\r\n";
-			valid = false;
-		}
-		if(document.getElementById('email').value.length == 0){
-			validationMsg = validationMsg + "Please fill out your email\r\n";
-			valid = false;
-		}
-		if(document.getElementById('username').value.length == 0){
-			validationMsg = validationMsg + "Please create a username\r\n";
-			valid = false;
-		}
-		if(document.getElementById('password').value.length == 0){
-			validationMsg = validationMsg + "Please create a password\r\n";
-			valid = false;
-		}
-		if(document.getElementById('cPassword').value.length == 0){
-			validationMsg = validationMsg + "Please confirm your password\r\n";
-			valid = false;
-		}
-		
-		if(valid == false){
-			alert(validationMsg);
-		}
-		
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-/*function validateNameElement(){
-	if(isFirstNameValid = false){
-		alert('error:fill out more');
-	}
-	else{
-		validateFirstName();
-	}
-}
-function validateFirstName(){
-	if( > 30){
-		alert('error:too many characters in first name');
-	}
-}		
-		var middleNameElement = document.getElementById("middlenam");
-		
-		if(x.length == 0 ){
-			alert('please enter in more information');
-		}
-		if(x.length > 30){
-			alert('you have entered too many letters for your name');
-		}
-		if(y.length == 0){
-			alert('please enter in more information');
-		}
-		if(y.length > 30){
-			alert('you have entered too many letters for your name');
-		}
-		
-		var firstNameValid = isFirstNameValid();
-
-function isFirstNameValid(){
-	var firstNameElement = document.getElementById("firstnam");
-	isEmpty(firstNameElement.length);
-	
-}
-*/
-
-
-
-
-
-
-
-
-
-=======
 		 var validationMsg = "Please correct the following errors: \r\n";
 		 var firstnam = document.getElementById('firstnam');
 		 var middleName = document.getElementById('middlenam');
@@ -194,34 +34,37 @@ function isFirstNameValid(){
 		 function validateFirstName(){
 			 if (validateRequired(firstnam.value.length, 0)== false)
 			 {
-			       
-			 } 
-			 else if(overMax(firstnam.value.length, 30)== false){
-
-			 }	
+				 
+			 }else if (underMin(firstnam.value.length, 2)== false){
+				 
+			 }
+			 if (overMax(firstnam.value.length, 30)== false){
+				 
+			 }
 	       }
 		 function validateMiddleName(){
-			 if(validateRequired(middleName.value.length, 0)== false){
-			
-			 } 	else if(overMax(middleName.value.length, 30)== false){
-			
+			 if(overMax(middleName.value.length, 30)== false){
+					
 			 }
 		 } 
 		 function validateLastName(){
 			 if(validateRequired(lastName.value.length, 0)== false){
 			
-			 } else if(overMax(lastName.value.length, 30)== false){
+			 } else if(underMin(lastName.value.length, 2)== false){
 			
+			 }
+			 if(overMax(lastName.value.length, 30)== false){
+					
 			 }
 		 }
 			 
 		function validateMonth(){ 
-			 if(validateRequired(month.value.length, 0)== false){
-			
-			 }else if(overMax(month.value.length, 8)== false){
-			
-			
-			 }
+			if (validateRequired(month.value.length, 0)== false){
+			}
+			else if (validateIfMonth(month.value)== false){
+				
+			}
+				
 		}
 			 
 		function validateDay(){	 
@@ -235,8 +78,11 @@ function isFirstNameValid(){
 		function validateYear(){	 
 			 if(validateRequired(year.value.length, 0)== false){
 			
-			 }else if(overMax(year.value.length, 4)== false){
+			 }else if(underMin(year.value.length, 4)== false){
 		
+			 }
+			 if(overMax(year.value.length, 4)== false){
+					
 			 }
 		}
 			 
@@ -255,15 +101,21 @@ function isFirstNameValid(){
 		function validateCity(){
 			 	if(validateRequired(city.value.length, 0)== false){
 			
-			 }else if(overMax(city.value.length, 30)== false){
+			 }else if(underMin(city.value.length, 3)== false){
 
 			 }
+			 	if(overMax(city.value.length, 30)== false){
+
+				 }	
 		}	 	
 		function validateZipcode(){	 	
 			 if(validateRequired(zipcode.value.length, 0)== false){
 			
-			 }else if(overMax(zipcode.value.length, 5)== false){
+			 }else if(underMin(zipcode.value.length, 5)== false){
 			
+			 }
+			 if(overMax(zipcode.value.length, 5)== false){
+					
 			 }
 		}	 
 		function validateEmail(){	 
@@ -276,24 +128,28 @@ function isFirstNameValid(){
 			 if(validateRequired(username.value.length, 0)== false){
 			
 			
-			 }else if(overMax(username.value.length, 15)== false){
+			 }else if(underMin(username.value.length, 6)== false){
 			
+			 }
+			 if(overMax(username.value.length, 15)== false){
+					
 			 }
 		}
 		function validatePassword(){	 
 			 if(validateRequired(password.value.length, 0)== false){
 			
 			
-			 }else if(overMax(password.value.length, 15)== false){
+			 }else if(underMin(password.value.length, 6)== false){
 			
+			 }
+			 if(overMax(password.value.length, 15)== false){
+				 
 			 }
 		}	 
 		function validateConPassword(){	 
 			 if(validateRequired(cPassword.value.length, 0)== false){
 			
-			 }else if(password.value != cPassword.value){
-			
-			 } 
+			 }
 		}	 
 			 
 		
@@ -314,5 +170,29 @@ function isFirstNameValid(){
 			validationMsg = validationMsg + "Please enter in less characters\r\n" ;
 			}
 		}
+		function underMin(variableV, minVal){
+			if(0 != variableV && variableV < minVal){
+				result = false;
+				validationMsg = validationMsg + "Please enter in more characters\r\n";
+				
+			}
+		}
+		function validateIfMonth(monthVal){
+			if(monthVal != "January" && monthVal != "Feburary" && monthVal != "March" && monthVal != "April" && monthVal != "May" && monthVal != "June" && monthVal != "July" && monthVal != "August" && monthVal != "September" && monthVal != "November" && monthVal != "December"){
+				
+				result = false;
+				validationMsg = validationMsg + "Please enter in a valid month\r\n";
+			}
+		}
+			
+		/*function confirmPass(passwordVal, cPasswordVal){
+			if(passwordVal != cPasswordVal){
+				result = false;
+				validationMsg = validationMsg + "Password does not match confirmation";
+			}
+			
+		}*/
+		
+		
 	}
 >>>>>>> refs/remotes/origin/master
