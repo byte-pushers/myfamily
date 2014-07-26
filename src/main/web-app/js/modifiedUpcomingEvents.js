422276276ed4a8dc68dcdbaccf9b64c8ec9eef1a
Jared <Jared@192.168.1.19>
2014-07-26 14:54:04 -0500

    Added finished button-array table. Not repaint method

--- src/main/web-app/js/modifiedUpcomingEvents.js
+++ src/main/web-app/js/modifiedUpcomingEvents.js
@@ -99,26 +99,26 @@ function customValidation() {
 }
 
 function attendeeValidation() {
-	var attendeeFError = document.getElementById("attendeeFError");
-	attendeeFError.innerHTML = "";
+	var attendeeFirstNameError = document.getElementById("attendeeFError");
+	attendeeFirstNameError.innerHTML = "";
 
-	var attendeeLError = document.getElementById("attendeeLError");
-	attendeeLError.innerHTML = "";
+	var attendeeLastNameError = document.getElementById("attendeeLError");
+	attendeeLastNameError.innerHTML = "";
 
 	var attendeeEmailError = document.getElementById("attendeeEmailError");
 	attendeeEmailError.innerHTML = "";
 
-	var aFNameElement = document.getElementById("afname");
-	var aLNameElement = document.getElementById("alname");
-	var aEmailElement = document.getElementById("aemail");
+	var attendeeFirstNameElement = document.getElementById("afname");
+	var attendeeLastNameElement = document.getElementById("alname");
+	var attendeeEmailElement = document.getElementById("aemail");
 	
 	/* ATTENDEE INFO */
-	textBoxValidation(aFNameElement, attendeeFError, 2, 10);
-	textBoxValidation(aLNameElement, attendeeLError, 2, 15);
-	emailValidation(aEmailElement, attendeeEmailError);
+	textBoxValidation(attendeeFirstNameElement, attendeeFirstNameError, 2, 10);
+	textBoxValidation(attendeeLastNameElement, attendeeLastNameError, 2, 15);
+	emailValidation(attendeeEmailElement, attendeeEmailError);
 
-	if (textBoxValidation(aFNameElement, attendeeFError, 2, 10) == true && textBoxValidation(aLNameElement, attendeeLError, 2, 15) == true
-			&& emailValidation(aEmailElement, attendeeEmailError) == true) {
+	if (textBoxValidation(attendeeFirstNameElement, attendeeFirstNameError, 2, 10) == true && textBoxValidation(attendeeLastNameElement, attendeeLastNameError, 2, 15) == true
+			&& emailValidation(attendeeEmailElement, attendeeEmailError) == true) {
 		attendeeCount++;
 		return true;
 	} 
@@ -127,17 +127,17 @@ function attendeeValidation() {
 }
 
 function addAttendee(){
-	var aFNameElement = document.getElementById("afname");
-	var aLNameElement = document.getElementById("alname");
-	var aEmailElement = document.getElementById("aemail");
+	var attendeeFirstNameElement = document.getElementById("afname");
+	var attendeeLastNameElement = document.getElementById("alname");
+	var attendeeEmailElement = document.getElementById("aemail");
 	
 	if(attendeeValidation() === true){
 		
 		var attendee = {
-			    firstName: aFNameElement.value,
-			    lastName : aLNameElement.value,
+			    firstName: attendeeFirstNameElement.value,
+			    lastName : attendeeLastNameElement.value,
 			    isDisplayedInTable: false,
-			    email    : aEmailElement.value,
+			    email    : attendeeEmailElement.value,
 			    getEmail : function (){return this.email},
 			    fullName : function (){return this.firstName + " " + this.lastName}
 		}
@@ -149,19 +149,38 @@ function addAttendee(){
 	
 }
 
-function deleteRow(row)
-{
-document.getElementById("myTable").deleteRow(row);
+function deleteRow(id, index){
+	array.splice(index, 1);
+	
+	var row = document.getElementById(id);
+    row.parentNode.removeChild(row);
+	
+    counter--;
+	//TODO: pass in row index
+	//TODO:     add row ID and attendee index to the onClick event
+	//TODO: delete row index from array
+	//TODO: repaint method to repaint table based on array
+	//TODO:     clear out table method
+	
+	//TODO: pass in row index and row ID 
+	//TODO:     add row ID to each row created 
+	//TODO:     add row ID and attendee index to the onClick event 
+	//TODO: delete row index from array 
+	//TODO: only delete that row based on row ID
 }
 
 function addToTable(){
 	
 	var table = document.getElementById("table");
 	var row = table.insertRow(counter + 1);
+	row.id = "row" + counter;
 	
 	var removeRow=document.createElement("BUTTON");
 	var text=document.createTextNode("Remove");
 	removeRow.appendChild(text);
+	removeRow.onclick = function(){
+		deleteRow(row.id, counter);
+	  };
 
 	var cell0 = row.insertCell(0)
 	var cell1 = row.insertCell(1);
@@ -169,19 +188,11 @@ function addToTable(){
 	var cell3 = row.insertCell(3);
 	
 	cell0.innerHTML = counter + 1;
-	cell1.innerHTML = getAttendeeName(array[counter]); 
-	cell2.innerHTML = getEmail(array[counter]);
+	cell1.innerHTML = array[counter].fullName(); 
+	cell2.innerHTML = array[counter].getEmail();
 	cell3.appendChild(removeRow);
 	
 	counter++;
-}
-
-function getAttendeeName(object) {
-	return object.fullName();
-}
-
-function getEmail(object){
-	return object.getEmail();
 }
 
 function textBoxValidation(element, error, min, max) {
@@ -298,8 +309,7 @@ function isRequired(required) {
 function isEmpty(targetObject) {
 	var result = true;
 
-	if (targetObject != null && targetObject != undefined
-			&& targetObject.length > 0) {
+	if (targetObject != null && targetObject != undefined && targetObject.length > 0) {
 		result = false;
 	}
 
