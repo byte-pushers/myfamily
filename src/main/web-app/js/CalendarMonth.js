a4f2e1c8d07dc6eed82c1d0e08bd376cde3a6e14
PhoebeBaham <phoebe.baham@gmail.com>
2014-08-01 18:11:56 -0500

    Almost completey finished my CalendarMonth.js but it needs debugging so is currently commented out of my signup.js page, also made my functions do one thing and one thing only by calling other methods inside my ifRequired method on my signup.js page

--- src/main/web-app/js/CalendarMonth.js
+++ src/main/web-app/js/CalendarMonth.js
@@ -1,18 +1,46 @@
-function CalendarMonth(){
-	var array = [January, Feburary, March, April, May, June, July, August, September, October, November, December,Jan, Feb, Mar, Apr, May, June, July, Aug, Sept, Oct, Nov, Dec]; 
+function CalendarMonth(name,abbrivatedName){ 
+	var longName = name,
+		shortName = abbrivatedName;
 	
-	function longName(  ){
-		
-	}
-	function shortName(  ){
-		
-	}
-	function getLongName(){
-		return this.longName;
-	}
-	function getShortName(){
-		return this.shortName;
-	}
-
- 
+	this.getLongName = function(){
+		return longName;
+	};
+	
+	this.getShortName = function(){
+		return shortName;
+	};
 }
+CalendarMonth.prototype.arrayOfMonths = [
+    new CalendarMonth("January","Jan"),
+    new CalendarMonth("Feburary","Feb"),
+    new CalendarMonth("March","Mar"),
+    new CalendarMonth("April","Apr"),
+    new CalendarMonth("May","May"),
+    new CalendarMonth("June","June"),
+    new CalendarMonth("July","July"),
+    new CalendarMonth("August","Aug"),
+    new CalendarMonth("September","Sept"),
+    new CalendarMonth("October","Oct"),
+    new CalendarMonth("November", "Nov"),
+    new CalendarMonth("December", "Dec")
+ ];
+CalendarMonth.prototype.isValidMonth = function(monthVal){
+	var result = false;
+	
+	//TODO: loop through the array of months
+	for(var i = 0;i < CalendarMonth.arrayOfMonths.length;i++){
+		//TODO: get the month string out of the array of months
+		//TODO: compare the month string to the monthVal parameter that is passed into the isValidMonth(monthVal) method
+		//TODO: when month string equals the monthVal parameter, set results variable to true  
+		result = (CalendarMonth.arrayOfMonths[i].getLongName() == monthVal || CalendarMonth.arrayOfMonths[i].getShortName() == monthVal)? true: false; 
+		//if the result is true then we will break out of the loop
+		elementLabelName = (elementLabelName == undefined || elementLabelName == null || elementLabelName.length == 0 )? "" : " into " + elementLabelName + ".";
+		if(result == true){
+			break;
+		}else{
+			alert( 'Please enter a valid birth month');
+		}
+	} 
+	
+	return result;
+};
--- src/main/web-app/js/signup.js
+++ src/main/web-app/js/signup.js
@@ -1,7 +1,3 @@
-	var calanderMonth={
-		"longName" : "", "shortName" : ""
-	};
-
 	function validateMyForm(){
 		 var validationMsg = {
 				 "msg": "Please correct the following errors: \r\n"
@@ -38,9 +34,7 @@
 		 validateConPassword();
 		 
 		 function validateFirstName(){
-			 validateRequired(firstNameInputTextElement.value.length, 0, validationMsg, "your first name");
-			 
-			 underMin(firstNameInputTextElement.value.length, 2, validationMsg, "your first name");
+			 validateRequired(firstNameInputTextElement.value.length, 2, 0, validationMsg, "your first name");
 			 
 			 overMax(firstNameInputTextElement.value.length, 30, validationMsg, "your first name");
 			 
@@ -48,17 +42,15 @@
 			
 		 }
 		 function validateMiddleName(){
-			 overMax(middleNameInputTextElement.value.length, 30, validationMsg, "your middle name");
+			 validateRequired(middleNameInputTextElement.value.length, 2, 0, validationMsg, "your middle name");
 			 
-			 underMin(middleNameInputTextElement.value.length, 2, validationMsg, "your middle name");
+			 overMax(middleNameInputTextElement.value.length, 30, validationMsg, "your middle name");
 			 
 			 ifValidMiddleName(middleNameInputTextElement.value, validationMsg);
 					
 		 } 
 		 function validateLastName(){
-			 validateRequired(lastNameInputTextElement.value.length, 0, validationMsg, "your last name");
-			
-			 underMin(lastNameInputTextElement.value.length, 2, validationMsg, "your last name");
+			 validateRequired(lastNameInputTextElement.value.length, 2, 0, validationMsg, "your last name");
 			
 			 overMax(lastNameInputTextElement.value.length, 30, validationMsg, "your last name");
 			
@@ -67,52 +59,46 @@
 		 }
 			 
 		function validateMonth(){ 
-			validateRequired(monthInputTextElement.value.length, 0, validationMsg, "your birth month");
-				
+			validateRequired(monthInputTextElement.value.length, 1, 0, validationMsg, "your birth month");
+	
+			/*CalendarMonth.isValidMonth(monthInputTextElement.value);*/
 			
-			validateIfMonth(monthInputTextElement.value, validationMsg);
 				
 			
 				
 		}
 			 
 		function validateDay(){	 
-			 validateRequired(dayInputTextElement.value.length, 0, validationMsg, "your birthday");
+			 validateRequired(dayInputTextElement.value.length, 1, 0, validationMsg, "your birthday");
 			validateIfDay(dayInputTextElement.value, validationMsg);
 			 
 		}
 			 
 		function validateYear(){	 
-			 validateRequired(yearInputTextElement.value.length, 0, validationMsg, "your birth year");
-			
-			 underMin(yearInputTextElement.value.length, 4, validationMsg, "birth year");
+			 validateRequired(yearInputTextElement.value.length, 4, 0, validationMsg, "your birth year");
 		
 			 overMax(yearInputTextElement.value.length, 4, validationMsg, "birth year");
 					
 		}
 			 
 		function validateCountry(){	 
-			 validateRequired(countrySelectDropDownMenu.value, 'choose', validationMsg, "your country");
+			 validateRequired(countrySelectDropDownMenu.value, 1, 'choose', validationMsg, "your country");
 			 
 		}	 
 		function validateState(){
-			 validateRequired(stateSelectDropDownMenu.value, 'choose', validationMsg, "your state");
+			 validateRequired(stateSelectDropDownMenu.value, 1, 'choose', validationMsg, "your state");
 			
 		}
 		function validateCity(){
-			 	validateRequired(cityInputTextElement.value.length, 0, validationMsg, "your city");
+			 	validateRequired(cityInputTextElement.value.length, 3, 0, validationMsg, "your city");
 			
-			 underMin(cityInputTextElement.value.length, 3, validationMsg, "your city");
-
 			 overMax(cityInputTextElement.value.length, 30, validationMsg, "your city");
 			 
 			 ifValidCity(cityInputTextElement.value, validationMsg);
 		}	 	
 		function validateZipcode(){	 	
-			 validateRequired(zipcodeInputTextElement.value.length, 0, validationMsg, "your zipcode");
-			
-			 underMin(zipcodeInputTextElement.value.length, 5, validationMsg, "your zipcode");
-			
+			 validateRequired(zipcodeInputTextElement.value.length, 5, 0, validationMsg, "your zipcode");
+			 
 			 overMax(zipcodeInputTextElement.value.length, 5, validationMsg, "your zipcode");
 			 
 			 ifValidZipcode(zipcodeInputTextElement.value, validationMsg);
@@ -120,15 +106,13 @@
 					
 		}	 
 		function validateEmail(){	 
-			 validateRequired(emailInputTextElement.value.length, 0, validationMsg, "your email");
+			 validateRequired(emailInputTextElement.value.length, 1, 0, validationMsg, "your email");
 			 
 			 validateIfValidEmail(emailInputTextElement.value, validationMsg);
 			 
 		}
 		function validateUsername(){
-			 validateRequired(usernameInputTextElement.value.length, 0, validationMsg, "your username");
-			
-			underMin(usernameInputTextElement.value.length, 6, validationMsg, "your username");
+			 validateRequired(usernameInputTextElement.value.length, 6, 0, validationMsg, "your username");
 			
 			 overMax(usernameInputTextElement.value.length, 15, validationMsg, "your username");
 			 
@@ -136,16 +120,14 @@
 					
 		}
 		function validatePassword(){	 
-			 validateRequired(passwordInputPasswordElement.value.length, 0, validationMsg, "your password");
-			
-			underMin(passwordInputPasswordElement.value.length, 6, validationMsg, "your password");
+			 validateRequired(passwordInputPasswordElement.value.length, 6, 0, validationMsg, "your password");
 			
 			 overMax(passwordInputPasswordElement.value.length, 15, validationMsg, "your password");
 			 
 			 ifValidPassword(passwordInputPasswordElement.value, validationMsg);
 		}	 
 		function validateConPassword(){	 
-			validateRequired(cPasswordInputPasswordElement.value.length, 0, validationMsg, "the confirm password textbox");
+			validateRequired(cPasswordInputPasswordElement.value.length,1, 0, validationMsg, "the confirm password textbox");
 			
 			confirmPassword(passwordInputPasswordElement.value.length,cPasswordInputPasswordElement.value.length, validationMsg);
 			
@@ -156,26 +138,34 @@
 			alert(validationMsg.msg);
 		}	
 	}
-	function validateRequired(valAndLen, defaultValue, validationMsg, elementLabelName){
-		elementLabelName = (elementLabelName == undefined || elementLabelName == null || elementLabelName.length == 0 )? "" : " " + elementLabelName + ".";
+	function isEmpty(value,defaultV){
+		if(value == defaultV){
+			return true;
+		}
 		
-		if (valAndLen == defaultValue){
+	}
+	function validateRequired(valAndLen, maxValue, defaultValue, validationMsg, elementLabelName){
+		if(isEmpty(valAndLen,defaultValue)==true){
+			elementLabelName = (elementLabelName == undefined || elementLabelName == null || elementLabelName.length == 0 )? "" : " " + elementLabelName + ".";
 			result = false;
 			validationMsg.msg += "Please fill out" + elementLabelName + "\r\n";
-		} 
+		}else{
+			underMin(valAndLen,maxValue, validationMsg, elementLabelName);
+		}
+		
 	}
 	function overMax(variableVal,maxVal, validationMsg, elementLabelName){
-		elementLabelName = (elementLabelName == undefined || elementLabelName == null || elementLabelName.length == 0 )? "" : "into " + elementLabelName + ".";
+		elementLabelName = (elementLabelName == undefined || elementLabelName == null || elementLabelName.length == 0 )? "" : " into " + elementLabelName + ".";
 		if(variableVal > maxVal){
 		result = false;
-		validationMsg.msg = validationMsg.msg + "Please enter less characters\r\n" ;
+		validationMsg.msg = validationMsg.msg + "Please enter less characters" + elementLabelName + "\r\n" ;
 		}
 	}
 	function underMin(variableV, minVal, validationMsg, elementLabelName){
 		elementLabelName = (elementLabelName == undefined || elementLabelName == null || elementLabelName.length == 0 )? "" : " into " + elementLabelName + ".";
-		if(0 != variableV && variableV < minVal){
+		if(variableV < minVal){
 			result = false;
-			validationMsg.msg = validationMsg.msg + "Please enter more characters\r\n";
+			validationMsg.msg = validationMsg.msg + "Please enter more characters" + elementLabelName + "\r\n";
 			
 		}
 	}
@@ -208,13 +198,13 @@
 			}
 		
 	}
-	function validateIfMonth(monthVal, validationMsg){
+	/*function validateIfMonth(monthVal, validationMsg){
 		if(monthVal != 0 && monthVal != "January" && monthVal != "Feburary" && monthVal != "March" && monthVal != "April" && monthVal != "May" && monthVal != "June" && monthVal != "July" && monthVal != "August" && monthVal != "September" && monthVal != "November" && monthVal != "December"){
 			
 			result = false;
 			validationMsg.msg = validationMsg.msg + "Please enter  a valid month\r\n";
 		} 
-	}
+	}*/
 	function validateIfDay(dayVal, validationMsg){
 		if(dayVal != 0 && dayVal != 1 && dayVal != 2 && dayVal != 3 && dayVal != 4 && dayVal != 5 && dayVal != 6 && dayVal != 7 && dayVal != 8 && dayVal != 9 && dayVal != 10 && dayVal != 11 && dayVal != 12 && dayVal != 13 && dayVal != 14 && dayVal != 15 && dayVal != 16 && dayVal != 17 && dayVal != 18 && dayVal != 19 && dayVal != 20 && dayVal != 21 && dayVal != 22 && dayVal != 23 && dayVal != 24 && dayVal != 25 && dayVal != 26 && dayVal != 27 && dayVal != 28 && dayVal != 29 && dayVal != 30 && dayVal != 31 ){
 			result = false;
