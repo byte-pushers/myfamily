932628829f03ec1046346be17315b0b557d722e5
Jared <Jared@Jared-PC.attlocal.net>
2014-08-12 15:15:45 -0500

    eventContoller update

--- src/main/web-app/js/angular/controller/eventController.js
+++ src/main/web-app/js/angular/controller/eventController.js
@@ -1,33 +1,38 @@
 app.controller("eventController", function($scope) {
-    $scope.eventNameInput = "";
-    $scope.descriptionInput = "";
-    $scope.htmlInput = "";
-    $scope.checkBox1Input = "";
-    $scope.checkBox2Input = "";
-    $scope.street1Input = "";
-    $scope.street2Input = "";
-    $scope.cityInput = "";
-    $scope.statesInput = "";
-    $scope.zipInput = "";
-    $scope.countryInput = "";
-    $scope.startMonthInput = "";
-    $scope.startDayInput = "";
-    $scope.startYearInput = "";
-    $scope.startHourInput = "";
-    $scope.startMinuteInput = "";
-    $scope.startMeridiemInput = "";
-    $scope.endMonthInput = "";
-    $scope.endDayInput = "";
-    $scope.endYearInput = "";
-    $scope.endHourInput = "";
-    $scope.endMinuteInput = "";
-    $scope.endMeridiemInput = "";
-    
-    $scope.createEvent  = function() {
-    	var event = new Event($scope.eventNameInput,
-    			$scope.descriptionInput, $scope.htmlInput, $scope.checkBox1Input, $scope.checkBox2Input, $scope.street1Input, $scope.street2Input, $scope.cityInput, 
-    			$scope.statesInput, $scope.zipInput, $scope.countryInput, $scope.startMonthInput, $scope.startDayInput, $scope.startYearInput, 
-    			$scope.startHourInput, $scope.startMinuteInput, $scope.startMeridiemInput, $scope.endMonthInput, $scope.endDayInput, $scope.endYearInput,
-    			$scope.endHourInput, $scope.endMinuteInput, $scope.endMeridiemInput);
-    };
+	$scope.eventNameInput = "";
+	$scope.descriptionInput = "";
+	$scope.htmlInput = "";
+	$scope.checkBox1Input = "";
+	$scope.checkBox2Input = "";
+	$scope.street1Input = "";
+	$scope.street2Input = "";
+	$scope.cityInput = "";
+	$scope.statesInput = "";
+	$scope.zipInput = "";
+	$scope.countryInput = "";
+	$scope.startMonthInput = "";
+	$scope.startDayInput = "";
+	$scope.startYearInput = "";
+	$scope.startHourInput = "";
+	$scope.startMinuteInput = "";
+	$scope.startMeridiemInput = "";
+	$scope.endMonthInput = "";
+	$scope.endDayInput = "";
+	$scope.endYearInput = "";
+	$scope.endHourInput = "";
+	$scope.endMinuteInput = "";
+	$scope.endMeridiemInput = "";
+
+	$scope.createEvent = function() {
+		var event = new Event($scope.eventNameInput, $scope.descriptionInput,
+				$scope.htmlInput, $scope.checkBox1Input, $scope.checkBox2Input,
+				$scope.street1Input, $scope.street2Input, $scope.cityInput,
+				$scope.statesInput, $scope.zipInput, $scope.countryInput,
+				$scope.startMonthInput, $scope.startDayInput,
+				$scope.startYearInput, $scope.startHourInput,
+				$scope.startMinuteInput, $scope.startMeridiemInput,
+				$scope.endMonthInput, $scope.endDayInput, $scope.endYearInput,
+				$scope.endHourInput, $scope.endMinuteInput,
+				$scope.endMeridiemInput);
+	};
 });
