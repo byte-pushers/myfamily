2ed8b5bb9648169ad8dceb2704706dd484a7feee
Carlos Ramirez <Carlos Ramirez@Jared>
2015-02-21 11:32:39 -0600

    Work on ui-router hello world

--- src/main/webapp/testApp.js
+++ src/main/webapp/testApp.js
@@ -1,19 +1,41 @@
-var myApp = angular.module("testApp", ['ui.router']);
+var myapp = angular.module('myapp', ["ui.router"]);
+myapp.config(function ($stateProvider, $urlRouterProvider) {
 
-myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/route1");
 
     $stateProvider
-        .stage('home', {
-            url: '/home',
-            templateUrl: 'angularTest.html'
-        })
-        .stage('nextPage', {
-            url: '/nextPage',
-            templateUrl: 'angularTest2.html'
-        });
-    });
+            .state('route1', {
+                url: "/route1",
+                templateUrl: "route1.html"
+            })
+            .state('route1.list', {
+                url: "/list",
+                templateUrl: "route1.list.html",
+                controller: function ($scope) {
+                    $scope.items = ["A", "List", "Of", "Items"];
+                }
+            })
 
-myApp.controller('testController', ['$scope', function ($scope) {
+            .state('route2', {
+                url: "/route2",
+                templateUrl: "route2.html"
+            })
+            .state('route2.list', {
+                url: "/list",
+                templateUrl: "route2.list.html",
+                controller: function ($scope) {
+                    $scope.things = ["A", "Set", "Of", "Things"];
+                }
+            });
+});
+
+myapp.controller('ctrl', function ($scope, $state) {
+    $scope.changeState = function () {
+        $state.go('route2');
+    };
+});
+
+myapp.controller('testController', ['$scope', function ($scope) {
         $scope.name = 'jared';
 
         $scope.redirect = function () {
