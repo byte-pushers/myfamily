5c4fcb8199d295fd00b923bdfdf178cd2bf3cb2f
jaredramirez <jaredramirez@me.com>
2015-03-10 18:02:38 -0500

    added new controller

---
+++ src/main/webapp/js/angular/controllers/eventOverviewController.js
@@ -0,0 +1,8 @@
+myFamilyApp.controller('eventOverviewController', [ '$scope', '$state',
+		function($scope, $state, global) {
+	/*if(global.eventArray.length == 0 ){
+		$scope.event = "Not created"
+	}else{
+		 $scope.event = global.eventArray[global.eventArray.length-1].toString();
+	}*/
+}]);
