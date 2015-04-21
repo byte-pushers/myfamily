b2fa80147e1701e9b017c9b2ca602364d344d195
Jared Ramirez <jaredramirez@me.com>
2015-04-21 17:29:03 -0500

    reset from previous commit

---
+++ src/main/webapp/js/angular/app.js
@@ -0,0 +1,37 @@
+var myFamilyApp = angular.module("myFamilyApp", [ "ui.router" ]);
+
+myFamilyApp.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise("home");
+
+	$stateProvider
+	.state('home', {
+	    url : "/home",
+	    templateUrl : "partials/home.ng.html",
+	    controller : 'homeController'
+	})
+	.state('eventForm', {
+		url : "/eventForm",
+		templateUrl : "partials/eventForm.ng.html",
+		controller : 'eventFormController'
+	})
+    .state('eventOverview', {
+		url : "/eventOverview",
+		templateUrl : "partials/eventOverview.ng.html",
+		controller : 'eventOverviewController'
+	})
+    .state('createUser', {
+        url : "/createUser",
+        templateUrl : "partials/createUser.ng.html",
+        controller : 'createUserController'
+    })
+    .state('userCreated', {
+        url : "/userCreated",
+        templateUrl : "partials/userCreated.ng.html",
+        controller : 'createUserController'
+    })
+    .state('familySearch', {
+        url : "/familySearch",
+        templateUrl : "partials/familySearch.ng.html"
+    });
+});
