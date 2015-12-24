8776117a4226e46a11d6b9d7e9db797803dccc95
Jared Ramirez <jared.ramirez@software.bytepushers>
2015-12-24 17:53:45 -0600

    added epoch date suppoprt

--- src/main/webapp/js/angular/controllers/software.bytepushers.controller.UsersProfileController.js
+++ src/main/webapp/js/angular/controllers/software.bytepushers.controller.UsersProfileController.js
@@ -2,10 +2,15 @@ myFamilyApp.controller('UserProfileController', [ '$scope', '$state', 'UserProfi
     function($scope,  $state, UserProfileService) {
 
         $scope.submit = function(isValid){
+            /*
+            * non-structural bugs:
+            *    date sent to server not current date
+             */
+
             configureUser();
+            console.log($scope.userProfileUIObject.person.birthDate);
 
             var user = new UserProfile($scope.getUserProfileUIObject());
-            console.log("user: " + user.toJSON());
             var requestParameters = {payload: user};
 
             if(isValid) {
@@ -76,7 +81,7 @@ myFamilyApp.controller('UserProfileController', [ '$scope', '$state', 'UserProfi
         };
 
         function getMockUser() {
-            return {"id":0,"username":"jaredramirez","password":"135132413","isAttendee":false,"person":{"id":0,"firstName":"Jared","middleName":null,"lastName":"Ramirez","emails":[{"id":0,"contactType":"PRIMARY","emailAddress":"jaredramirez@me.com","createdDate":1450045104519,"lastModifiedDate":null,"createdBy":"jared","lastModifiedBy":"jared"}],"birthDate":[1997,3,30],"phoneNumbers":[{"id":null,"contactType":"PRIMARY","country":"CANADA","areaCode":"940","exchangeCode":"368","localNumber":"7410","createdDate":"2015-10-1 3:00 PM GMT+1:00","lastModifiedDate":"2015-10-1 3:00 PM GMT+1:00","createdBy":"jared","lastModifiedBy":"jared"}],"addresses":[{"id":null,"contactType":"PRIMARY","street1":"1004WestOak","street2":null,"city":"Denton","state":"TEXAS","zip":"76201","country":"CANADA","createdDate":"2015-10-1 3:00 PM GMT+1:00","lastModifiedDate":"2015-10-1 3:00 PM GMT+1:00","createdBy":"jared","lastModifiedBy":"jared"}],"gender":"MALE","createdDate":"2015-10-1 3:00 PM GMT+1:00","lastModifiedDate":"2015-10-1 3:00 PM GMT+1:00","createdBy":"jared","lastModifiedBy":"jared"},"createdDate":"2015-10-1 3:00 PM GMT+1:00","lastModifiedDate":"2015-10-1 3:00 PM GMT+1:00","createdBy":"jared","lastModifiedBy":"jared"};
+            return {"id":0,"username":"jaredramirez","password":"135132413","isAttendee":false,"person":{"id":0,"firstName":"Jared","middleName":null,"lastName":"Ramirez","emails":[{"id":0,"contactType":"PRIMARY","emailAddress":"jaredramirez@me.com","createdDate":1450045104519,"lastModifiedDate":1450045104519,"createdBy":"jared","lastModifiedBy":"jared"}],"birthDate":[1997,3,30],"phoneNumbers":[{"id":0,"contactType":"PRIMARY","country":"CANADA","areaCode":"940","exchangeCode":"368","localNumber":"7410","createdDate":1450045104519,"lastModifiedDate":1450045104519,"createdBy":"jared","lastModifiedBy":"jared"}],"addresses":[{"id":0,"contactType":"PRIMARY","street1":"1004WestOak","street2":null,"city":"Denton","state":"TEXAS","zip":"76201","country":"CANADA","createdDate":1450045104519,"lastModifiedDate":1450045104519,"createdBy":"jared","lastModifiedBy":"jared"}],"gender":"MALE","createdDate":1450045104519,"lastModifiedDate":1450045104519,"createdBy":"jared","lastModifiedBy":"jared"},"createdDate":1450045104519,"lastModifiedDate":1450045104519,"createdBy":"jared","lastModifiedBy":"jared"};
         }
     }
 ]);
